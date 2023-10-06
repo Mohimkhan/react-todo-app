6a64260169e9883ac3486fab4b5f2cf4498d5ce1
Mohim Khan <mohimkhan652@gmail.com>
2023-10-06 19:47:25 +0600

    added Footer component in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
-import Header from '../components/header/Header';
-import ThemeProvider from '../contexts/ThemeContext.jsx';
 import '../styles/globalStyles/global.css';
 import '../styles/themeStyles/dark.css';
 import '../styles/themeStyles/light.css';
+import ThemeProvider from '../contexts/ThemeContext.jsx';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
 
 const Home = () => {
    return (
       <>
          <ThemeProvider>
             <Header />
+            <Footer />
          </ThemeProvider>
       </>
    );
