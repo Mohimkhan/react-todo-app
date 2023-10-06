7442086535a5d18962cb368bb4de50f899514797
Mohim Khan <mohimkhan652@gmail.com>
2023-10-06 19:33:07 +0600

    added Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import Header from '../components/header/Header';
+import ThemeProvider from '../contexts/ThemeContext.jsx';
+import '../styles/globalStyles/global.css';
+import '../styles/themeStyles/dark.css';
+import '../styles/themeStyles/light.css';
+
 const Home = () => {
-   return <div>Home</div>;
+   return (
+      <>
+         <ThemeProvider>
+            <Header />
+         </ThemeProvider>
+      </>
+   );
 };
 
 export default Home;
